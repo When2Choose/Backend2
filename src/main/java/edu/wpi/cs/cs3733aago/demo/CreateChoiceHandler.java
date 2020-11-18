f310dd9829d46c4c9aea9662be1cfba6333d4372
agalgano08 <agalgano@wpi.edu>
2020-11-18 12:46:00 -0500

    initial commit

---
+++ src/main/java/edu/wpi/cs/cs3733aago/demo/CreateChoiceHandler.java
@@ -0,0 +1,109 @@
+package edu.wpi.cs.cs3733aago.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.util.json.Jackson;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class CreateChoiceHandler implements RequestStreamHandler {
+
+	LambdaLogger logger;
+
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		logger = context.getLogger();
+		if (context != null) {
+			context.getLogger();
+		}
+
+		// load entire input into a String (since it contains JSON)
+		StringBuilder incoming = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(input))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				incoming.append(line);
+			}
+		}
+
+		// When coming in from Lambda function is pure JSON. When coming from API
+		// Gateway or the
+		// real thing, then is wrapped inside more complicated JSON and you only want
+		// the BODY
+		// in most cases.
+		JsonNode node = Jackson.fromJsonString(incoming.toString(), JsonNode.class);
+
+		if (node.has("body")) {
+			node = Jackson.fromJsonString(node.get("body").asText(), JsonNode.class);
+		}
+
+		int groupMembers;
+		String description, alternatives;
+
+		String param = node.get("groupMembers").asText();
+		groupMembers = Integer.parseInt(param);
+		param = node.get("description").asText();
+		description = param;
+		param = node.get("alternatives").asText();
+		alternatives = param;
+
+//	    	boolean error = false;
+//			try {
+//	    		arg1 = Double.parseDouble(param);
+//	    	} catch (NumberFormatException nfe) {
+//	    		try {
+//	    			arg1 = getDoubleFromBucket(param);
+//	    		} catch (Exception e) {
+//	    			logger.log("Unable to parse:" + param + " as arg1"); 
+//	    			error = true;
+//	    		}
+//	    	}
+
+//			param = node.get("arg2").asText();
+//			try {
+//	    		arg2 = Double.parseDouble(param);
+//			} catch (NumberFormatException nfe) {
+//	    		try {
+//	    			arg2 = getDoubleFromBucket(param);
+//	    		} catch (Exception e) {
+//	    			logger.log("Unable to parse:" + param + " as arg2"); 
+//	    			error = true;
+//	    		}
+//	    	}
+//	    	
+//	        PrintWriter pw = new PrintWriter(output);
+//
+//	        int statusCode;
+//	        double sum = 0;
+//			if (error) {
+//				statusCode = 400;
+//			} else {
+//		    	sum = arg1 + arg2; 
+//		    	statusCode = 200;
+//			}
+
+		PrintWriter pw = new PrintWriter(output);
+
+		int statusCode = 2001;
+		String responeString = Integer.toString(groupMembers) + " " + alternatives + " " + description;
+
+		// Needed for CORS integration...
+		String response = "{ \n" + "  \"isBase64Encoded\" : false, \n" + "  \"statusCode\"      : " + statusCode
+				+ ", \n" + "  \"headers\" : { \n " + "     \"Access-Control-Allow-Origin\" : \"*\", \n"
+				+ "     \"Access-Control-Allow-Method\"  : \"GET,POST,OPTIONS\" \n" + "  }, \n" + "  \"body\" : \""
+				+ "{ \\\"result\\\" : \\\"" + responeString + "\\\" }" + "\" \n" + "}";
+
+		// write out.
+		pw.print(response);
+		pw.close();
+	}
+}
+
+//}
