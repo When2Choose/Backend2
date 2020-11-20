70b536fd57669b11164987fa166c82ec359e89de
agalgano08 <agalgano@wpi.edu>
2020-11-19 22:09:03 -0500

    Latest version of API

--- src/main/java/edu/wpi/cs/cs3733io/db/ChoicesDAO.java
+++ src/main/java/edu/wpi/cs/cs3733io/db/ChoicesDAO.java
@@ -1,11 +1,18 @@
 package edu.wpi.cs.cs3733io.db;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.UUID;
+
+import edu.wpi.cs.cs3733io.db.DatabaseUtil;
+import edu.wpi.cs.cs3733io.model.Choice;
 
 public class ChoicesDAO {
 
 	java.sql.Connection conn;
 	
-	final String tblName = "Constants";   // Exact capitalization
+	final String tblName = "choices";   // Exact capitalization
 
     public ChoicesDAO() {
     	try  {
@@ -15,5 +22,36 @@ public class ChoicesDAO {
     	}
     }
 
+    public Choice getConstant(String name) throws Exception {
+        
+        try {
+            Choice choice = null;
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " + tblName + " WHERE name=?;");
+            ps.setString(1,  name);
+            ResultSet resultSet = ps.executeQuery();
+            
+            while (resultSet.next()) {
+            	choice = generateChoice(resultSet);
+            }
+            resultSet.close();
+            ps.close();
+            
+            return choice;
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+            throw new Exception("Failed in getting choice: " + e.getMessage());
+        }
+    }
+
+    private Choice generateChoice(ResultSet resultSet) throws Exception {
+        int memberCount  = resultSet.getInt("memberCount");
+        String description = resultSet.getString("description");
+        String dateCompleted = resultSet.getString("dateCompleted");
+        String uuid = resultSet.getString("uuid");
+        LinkedList<String> alternativeNames = new LinkedList<String>();
+        
+        return new Choice (uuid, memberCount, description, dateCompleted, alternativeNames);
+    }
 	
 }
